fa35e7d6691dfb526f4390e0270df0990b766e44
Andrew Luehrs <45299090+andreios@users.noreply.github.com>
2021-04-16 09:46:17 -0500

    Add files via upload
    C# Console solution for testing maze data structure and algorithms.

---
+++ Cell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazePractice
+{
+    class Cell
+    {
+        //members
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Cell North { get; set; }
+        public Cell South { get; set; }
+        public Cell East { get; set; }
+        public Cell West { get; set; }
+
+    }
+}
